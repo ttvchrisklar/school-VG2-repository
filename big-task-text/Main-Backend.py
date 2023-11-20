d0e9b527cb5657f58e826aa5196b38f27b2d45d4
ttvchrisklar <106084524+ttvchrisklar@users.noreply.github.com>
2023-11-20 13:27:17 +0100

    .

--- big-task-text/Main-Backend.py
+++ big-task-text/Main-Backend.py
@@ -1,5 +1,8 @@
-clean_data = []
+clean_product_data = []
 productlist = []
+customerlist = []
+orderList = []
+orderclasslist = []
 # class liste.
 class product:
     def __init__(self, name, description, price, amount, productID):
@@ -8,6 +11,7 @@ class product:
         self.description = description
         self.productID = productID
         self.amount = amount
+
 class customer:
     def __init__(self, name, compony, billingadress, deliveryadress, orderhistery):
         self.name = name
@@ -16,11 +20,21 @@ class customer:
         self.deliveryadress = deliveryadress
         self.orderhistery = orderhistery
 
+class order:
+    def __init__(self, orderdescrition, productID, amount, fromLocation, toLocation, cost, status):
+        self.productID = productID
+        self.amount = amount
+        self.fromLocation = fromLocation
+        self.toLocation = toLocation
+        self.cost = cost
+        self.status = status 
+        self.orderdescrition = orderdescrition
+
 def directory():
     # dette er directory som gir brukeren mulighet til og bevege seg i programet.
     print("\n\ndirectory:")
-    print("Produktadministrasjon [P]\nOrdrebehandling [O]\nKundehåndtering [K]\nRapporter [R]\nHelp [help]\nExit [exit]")
-    directoryorder = input("hvor skal du: ")
+    print("Produktadministrasjon [P]\nOrdrebehandling [O]\nKundehåndtering(WIP) [K]\nRapporter(WIP) [R]\nHelp(WIP) [help]\nExit [exit]")
+    directoryorder = input("hva skal du: ")
     directoryorder = str(directoryorder.upper())
     match directoryorder:
         case "P":
@@ -44,29 +58,39 @@ def directory():
 def Produktadministrasjon():
     # dette er får administrasjon av produkter i systemet.
     print("\nProduktadministrasjon:")
-    adminorder = input("hvor skal du: Vare navn [VN], Vare beskrivelse [VB], Vare pris [VP],vare antall [VA] , alle varer [AV], tilbake [B]:")
+    adminorder = input("hvor skal du: Vare navn [VN], Vare beskrivelse [VB], Vare pris [VP],vare antall [VA] , alle varer [AV], ful vare beskrivelse[FVB] tilbake [B]:")
     adminorder = str(adminorder.upper())
-    match adminorder:
-        case "VN":
-            productcheker(adminorder)
-        case "VB":
-            productcheker(adminorder)
-        case "VP":
-            productcheker(adminorder)
-        case "VA":
-            productcheker(adminorder)
-        case "AV":
-            productcheker(adminorder)
+    valid_cases = ["VN", "VB", "VP", "VA", "AV", "FVB"]
+    if adminorder in valid_cases:
+        productcheker(adminorder)
+    elif adminorder == "B":
+        directory()
+    else:
+        print("prøv på nytt")
+        Produktadministrasjon()
+
+def Ordrebehandling():
+    # Ordrebehandling av kundens bestilinger og bedriftens bestilinger.
+    print("\nOrdrebehandling")
+    director = input("\n vil du:\n Ny bestiling til lager [OI]\n Ny bestiling til kunde [OU]\n Alle ordere [AO]\n tilbake[B]\n ")
+    director = str(director.upper())
+    match director:
+        case "OI":
+            print("Ny bestiling til lager")
+            # make input methid for name of the order, order ID, amount, from, to, cost?, states?
+        case "OU":
+            print("Ny bestiling til kunde")
+            # make input methid for name of the order, order ID, amount, from, to, cost?, states?
+        case "AO":
+            print("Alle ordere")
+            for i in range(len(orderclasslist)):
+                print("--------\n"+orderclasslist[i].orderdescrition +"\n"+ orderclasslist[i].productID +"\n"+ orderclasslist[i].amount+"\n"+orderclasslist[i].fromLocation +"\n"+ orderclasslist[i].toLocation+"\n"+ orderclasslist[i].cost +" NOK\n"+orderclasslist[i].status)
+            Ordrebehandling()
         case "B":
             directory()
         case _:
-            print("prøv på nytt")
-            Produktadministrasjon()
-
-
-def Ordrebehandling():
-    # Ordrebehandling av kundens bestilinger og befriftens bestilinger.
-    print("\nOrdrebehandling")
+            print("ERROR, try agin")
+            Ordrebehandling()
 
 def Kundehåndtering():
     # Kundehåndtering av inormasjonen til kunder alt fra leverings adrese til facturering og ordre.
@@ -77,59 +101,104 @@ def Rapporter():
     print("\nRapporter")
 
 
-def productfillereader():
+def fillereader():
     with open('./big-task-text/text-files/products.txt','r', encoding='utf-8') as f:
-        data = f.readlines()
-    datacliner(data)
+        product_data = f.readlines()
+        f.close()
+    # with open('./big-task-text/text-files/customers.txt','r', encoding='utf-8') as f:
+    #     customer_data = f.readlines()
+    #     f.colse()
+    with open('./big-task-text/text-files/ordere.txt','r', encoding='utf-8') as o:
+        order_data = o.readlines()
+        o.close()
+    orderdatacliner(order_data)
+    productdatacliner(product_data)
 
-def datacliner(unclean_data):
+def customerDataCleaner(inn_data):
+    words_to_remove = ["Navn:", "Bedrift:", "Bakgrun:", "Sendingsadresse:", "Handlehistorie:"]
+    for item in inn_data:
+        for word in words_to_remove:
+            item = item.replace(word, "").strip()
+        clean_product_data.append(item)
+
+def productdatacliner(unclean_data):
     words_to_remove = ["Navn:", "Beskrivelse:", "Pris:", "VareID:", "Antall:"]
     for item in unclean_data:
         for word in words_to_remove:
             item = item.replace(word, "").strip()
-        clean_data.append(item)
+        clean_product_data.append(item)
 
-def productclassasembeler():
-    productfillereader()
-    while len(clean_data)!=0:
+def orderdatacliner(unclean_data):
+    words_to_remove = ["VareID:", "Mengde:", "Fra:", "Til:", "Pris:", "Status:", "Bestilingsbeskrivelse:"]
+    for item in unclean_data:
+        for word in words_to_remove:
+            item = item.replace(word, "").strip()
+        orderList.append(item)    
+    
+def dataclassasembeler():
+    fillereader()
+    while len(clean_product_data)!=0:
         i=0
-        newproduct = product(clean_data[i],clean_data[i+1],clean_data[i+2],clean_data[i+3],clean_data[i+4])
+        newproduct = product(clean_product_data[i],clean_product_data[i+1],clean_product_data[i+2],clean_product_data[i+3],clean_product_data[i+4])
         productlist.append(newproduct)
-        clean_data.pop(i+4)
-        clean_data.pop(i+3)
-        clean_data.pop(i+2)
-        clean_data.pop(i+1)
-        clean_data.pop(i)
-
+        clean_product_data.pop(i+4)
+        clean_product_data.pop(i+3)
+        clean_product_data.pop(i+2)
+        clean_product_data.pop(i+1)
+        clean_product_data.pop(i)
+    while len(orderList)!=0:
+        i=0
+        neworder = order(orderList[i],orderList[i+1],orderList[i+2],orderList[i+3],orderList[i+4],orderList[i+5],orderList[i+6])
+        orderclasslist.append(neworder)
+        orderList.pop(i+6)
+        orderList.pop(i+5)
+        orderList.pop(i+4)
+        orderList.pop(i+3)
+        orderList.pop(i+2)
+        orderList.pop(i+1)
+        orderList.pop(i)
+    
 def productcheker(order):
+    oID=0
     if order != "AV":
+        #rquest a product id number from the user
         pID = input("skriv product ID:" )
         pID = int(pID)
+        # then loop the list to find the ID number
         for i in range(len(productlist)):
             if pID == int(productlist[i].productID):
-                print("found it!")
+                oID = i
                 break
         else:
             print("product fines ikke, prøv på nyt")
             productcheker(order)
     match order:
         case "VN":
-            productcheker(adminorder)
+            # tells the name of the product
+            print(productlist[oID].name)
         case "VB":
-            productcheker(adminorder)
+            #description of the product
+            print(productlist[oID].description)
         case "VP":
-            productcheker(adminorder)
+            #price of the prduct
+            print(productlist[oID].price+" NOK")
         case "VA":
-            productcheker(adminorder)
+            #the amount of products in stock
+            print(productlist[oID].amount)
+        case "FVB":
+            #prints the full prodects "class"
+            print("--------\n"+productlist[oID].name +"\n"+ productlist[oID].description +"\n"+ productlist[oID].price+" NOK" +"\n"+productlist[oID].amount +"\n")
         case "AV":
+             #prints every produckt in the database.
              for i in range(len(productlist)):
-                 print(productlist[i].name +"\n"+ productlist[i].description +"\n"+ productlist[i].price +"\n"+productlist[i].amount +"\n"+ productlist[i].productID )
-        
-    
-    
+                 print("--------\n"+productlist[i].name +"\n"+ productlist[i].description +"\n"+ productlist[i].price+" NOK" +"\n"+productlist[i].amount +"\n"+ productlist[i].productID)
+        case _:
+            # error in case the adminorder gets removed mid manigment some how, idk how?
+            print("error, prøv på nytt")
+    Produktadministrasjon()
 
 def onstart():
-    productclassasembeler()    
+    dataclassasembeler()    
     directory()
 
 onstart()
